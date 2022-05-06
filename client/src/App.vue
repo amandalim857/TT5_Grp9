f39c58fbef789a9eaffaeadb7a64c1aab18a372d
diont <dion.teo.2018@smu.edu.sg>
2022-05-06 14:46:05 +0800

    resolve conflicts

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,37 +1,18 @@
 <template>
   <div id="app">
-<<<<<<< Updated upstream
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <LoginPage></LoginPage>
-    <Dashboard></Dashboard>
-=======
     <EachProject/>
->>>>>>> Stashed changes
   </div>
 </template>
 
 <script>
-<<<<<<< Updated upstream
-import Dashboard from '@/components/Dashboard.vue'
-import HelloWorld from './components/HelloWorld.vue'
-import LoginPage from './components/Login.vue'
-=======
 // import Dashboard from '@/components/Dashboard.vue'
 // import HelloWorld from './components/HelloWorld.vue'
 import EachProject from './components/Project.vue'
->>>>>>> Stashed changes
 
 export default {
   name: 'App',
   components: {
-<<<<<<< Updated upstream
-    HelloWorld,
-    Dashboard,
-    LoginPage
-=======
     EachProject
->>>>>>> Stashed changes
   }
 }
 </script>
