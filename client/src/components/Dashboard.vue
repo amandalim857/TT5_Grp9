c95e584598e1c9a296a7bc08ad2a2aab91715120
Amanda <amandalim857@gmail.com>
2022-05-06 12:30:09 +0700

    Add dashboard page

---
+++ client/src/components/Dashboard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <table>
+    <tr>
+        <th>ID</th>
+        <th>User ID</th>
+        <th>Name</th>
+        <th>Budget</th>
+        <th>Description</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>4</td>
+        <td>posb</td>
+        <td>3434</td>
+        <td>Hello hello</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>3</td>
+        <td>dbs</td>
+        <td>34315</td>
+        <td>Hello hello hello</td>
+    </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashBoard',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+   <table>
+        <tr>
+            <th>ID</th>
+            <th>User ID</th>
+            <th>Budget</th>
+            <th>Desription</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>4</td>
+            <td>1200000</td>
+            <td>Realtime Face Recognition</td>
+        </tr>
+        <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashBoard',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
