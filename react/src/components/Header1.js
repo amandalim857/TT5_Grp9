df92496ad04bc51ce216c607ee4b560de22c9b37
huangnq96 <huangnq1996@gmail.com>
2022-05-06 13:51:33 +0800

    add react part

---
+++ react/src/components/Header1.js
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import {useNavigate} from "react-router-dom"
+
+function Header1(props) {
+  const { title } = props;
+
+  const handleSignUp = ()=> {
+    
+  }
+
+  const handleLogin = ()=> {
+    
+  }
+
+  return (
+    <React.Fragment>
+      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          sx={{ flex: 1 }}
+        >
+          {title}
+        </Typography>
+
+        <Button onclick={handleLogin} variant="outlined" size="small">
+          Login
+        </Button>
+        <Button onclick={handleSignUp} variant="outlined" size="small">
+          Sign up
+        </Button>
+      </Toolbar>
+      <Toolbar
+        component="nav"
+        variant="dense"
+        sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
+      >
+      </Toolbar>
+    </React.Fragment>
+  );
+}
+
+Header1.propTypes = {
+  sections: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Header1;
